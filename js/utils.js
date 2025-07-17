d6cf26fa77151d21fb27fbba8a493cd68eaf600c
TheEgglet <woodstuffthings@gmail.com>
2025-07-17 17:38:12 +0200

    add jsdoc

--- js/utils.js
+++ js/utils.js
@@ -1,2 +1,11 @@
+/**
+ * Acquires an element.
+ * @param {string} id
+ */
 const $ = id => document.getElementById(id);
+/**
+ * Returns a random number.
+ * @param {number} min
+ * @param {number} max
+ */
 const rng = (min, max) => Math.floor(Math.random() * ((max + 1) - min)) + min;
