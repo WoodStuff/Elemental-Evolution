8dd3d169537bfaf48483d0548848b2dcc37644ca
TheEgglet <woodstuffthings@gmail.com>
2026-06-04 16:57:24 +0200

    move hydrogen tab back into Game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useHydrogen } from '../composables/useHydrogen';
-import EnergyTab from './energy/EnergyTab.vue';
-import HydrogenTab from './HydrogenTab.vue';
+import { Upgrade } from '../composables/useUpgrade';
+import EnergyTab from '../components/energy/EnergyTab.vue';
+import GatherButton from '../components/GatherButton.vue';
+import UpgradeButton from '../components/UpgradeButton.vue';
 
-const { total } = useHydrogen();
+const { amount, highest, upEfficiency, upCooldown } = useHydrogen();
+
+const upgrades: Upgrade[] = [upEfficiency, upCooldown];
+const filteredUpgrades = computed(() => upgrades.filter(up => {
+	if (up === upEfficiency) return highest.value >= 10;
+	if (up === upCooldown) return highest.value >= 50;
+}));
 </script>
 
 <template>
 	<div id="game">
-		<HydrogenTab />
+		<div class="element-tab">	
+			<p class="big">You have {{ amount }} hydrogen</p>
+			<GatherButton />
 
-		<EnergyTab v-if="total > 1500" />
+			<div class="upgrades" v-if="filteredUpgrades.length > 0">
+				<UpgradeButton v-for="up in filteredUpgrades" :type="up" />
+			</div>
+		</div>
+
+		<EnergyTab />
 	</div>
 </template>
 
@@ -18,5 +34,19 @@ const { total } = useHydrogen();
 #game {
 	display: flex;
 	gap: 100px;
+
+	& > .element-tab {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 16px;
+
+		& > div.upgrades {
+			display: flex;
+			gap: 8px;
+
+			margin-top: 16px;
+		}
+	}
 }
 </style>
